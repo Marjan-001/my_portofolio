1cc796bf99f41aa746919494338cadd86115fd97
Marjan-001 <marjan199721@gmail.com>
2022-12-10 22:29:56 +0600

    final

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,39 @@
 import '../styles/globals.css'
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    AOS.init({
+      once: false,
+      mirror: true,
+      duration: 3000
+    });
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>AOS</title>
+        <link
+          id="external-css"
+          rel="stylesheet"
+          type="text/css"
+          href="https://fonts.googleapis.com/css?family=Quicksand&amp;display=swap"
+          media="all"
+        />
+        <link
+          id="external-css"
+          rel="stylesheet"
+          type="text/css"
+          href="https://fonts.googleapis.com/css?family=Poppins&amp;display=swap"
+          media="all"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
